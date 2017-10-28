e9a97606b744c83c759c30208b02c499946e3efc
Andrey Kudakov <kudakovandrey@gmail.com>
2017-10-28 12:52:44 +0300

    [ex1-10]

---
+++ local/templates/exam1/page_templates/.content.php
@@ -0,0 +1,4 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?><?
+IncludeTemplateLangFile(__FILE__);
+$TEMPLATE["inner.php"] = Array("name"=>GetMessage("INNER"), "sort"=>2);
+?>
