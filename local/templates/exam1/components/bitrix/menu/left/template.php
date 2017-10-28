316c3bd8b303716021446814dc3965bce1af7a9a
Andrey Kudakov <kudakovandrey@gmail.com>
2017-10-28 22:41:46 +0300

    [ex1-25]

--- local/templates/exam1/components/bitrix/menu/left/template.php
+++ local/templates/exam1/components/bitrix/menu/left/template.php
@@ -1,7 +1,7 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?if (!empty($arResult)):?>
 <div class="side-block side-menu">
-	<div class="title-block"><?=GetMessage("T_EXAM_MENU_TITLE")?></div>
+	<div class="title-block <?$APPLICATION->ShowProperty('color_left_menu')?>"><?=GetMessage("T_EXAM_MENU_TITLE")?></div>
 	<div class="menu-block">
 		<ul>
 		<?
