dedafa21aaee2aeadc63f6956d1730e3d79681b3
Andrey Kudakov <kudakovandrey@gmail.com>
2017-10-28 11:28:49 +0300

    [ex1-1]

---
+++ partneram/.left.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Условия сотрудничества", 
+		"/partneram/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Как стать партнером", 
+		"/partneram/kak-stat-partnerom.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Расписание мероприятий", 
+		"/partneram/raspisanie-meropriyatiy/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
