834c568a7b596ca316f0848528a623462d88b5ce
Andrey Kudakov <kudakovandrey@gmail.com>
2017-10-28 22:10:16 +0300

    [ex1-18]

--- company/.left.menu.php
+++ company/.left.menu.php
@@ -1,40 +1,46 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?
 $aMenuLinks = Array(
-    Array(
-        "О компании",
-        "./",
-        Array(),
-        Array(),
-        ""
-    ),
-    Array(
-        "Руководство",
-        "management.php",
-        Array(),
-        Array(),
-        ""
-    ),
-    Array(
-        "Миссия и стратегия",
-        "mission.php",
-        Array(),
-        Array(),
-        ""
-    ),
-    Array(
-        "История",
-        "history.php",
-        Array(),
-        Array(),
-        ""
-    ),
-    Array(
-        "Вакансии",
-        "vacancies.php",
-        Array(),
-        Array(),
-        ""
-    )
+	Array(
+		"О компании", 
+		"./", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Руководство", 
+		"management.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Миссия и стратегия", 
+		"mission.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"История", 
+		"history.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Вакансии", 
+		"vacancies.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Обратная связь", 
+		"/company/obratnaya-svyaz.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
 );
 ?>
---
+++ company/obratnaya-svyaz.php
@@ -0,0 +1,19 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Обратная связь");
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:main.feedback", 
+	".default", 
+	array(
+		"EMAIL_TO" => "kudakovandrey@gmail.com",
+		"EVENT_MESSAGE_ID" => array(
+			0 => "29",
+		),
+		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
+		"REQUIRED_FIELDS" => array(
+		),
+		"USE_CAPTCHA" => "Y",
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
